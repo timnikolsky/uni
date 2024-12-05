a846df1a7baedf5bd3772d4abff43d49e381dfd5
Tim <tima@nopublic.ru>
2024-12-05 08:59:05 +0300

    feat(gemini-bot): premium features

--- apps/gemini-bot/src/main.js
+++ apps/gemini-bot/src/main.js
@@ -53,13 +53,19 @@ bot.on('ready', () => {
 
 bot.on('message', async (message) => {
 	if (!message.text) return
+	const hasPremium = await checkPremiumStatus(message.sender.id)
 
 	if (message.text.startsWith('/start')) {
-		message.chat.sendMessage('Добро пожаловать!')
+		message.chat.sendMessage('Добро пожаловать! Чтобы пообщаться с искуственным интеллектом, просто отправьте сообщение с запросом.')
 		return
 	}
 
-	if (message.text.startsWith('/subscribe')) {
+	if (message.text.startsWith('/premium')) {
+		if (hasPremium) {
+			message.chat.sendMessage('☺️ У вас уже оформлена подписка! Чтобы отменить подписку, перейдите в настройки Telegram и в разделе Telegram Stars отмените подписку.')
+			return
+		}
+
 		const invoiceLink = await bot.rest.request('createInvoiceLink', {
 			title: 'Timbot Premium',
 			description: 'Подписка на улучшенные модели GPT',
@@ -69,7 +75,7 @@ bot.on('message', async (message) => {
 			subscription_period: 2592000
 		})
 
-		message.chat.sendMessage('Купить подписку:', {
+		message.chat.sendMessage('Премиум подписка позволит вам выбирать более продвинутые модели нейронной сети, а также увеличит лимиты по длине запросов и ответов', {
 			replyMarkup: new InlineKeyboardMarkup({
 				inlineKeyboard: [[
 					new InlineKeyboardButton({
@@ -82,43 +88,76 @@ bot.on('message', async (message) => {
 		return
 	}
 
+	if (message.text.startsWith('/model')) {
+		if (!hasPremium) {
+			message.chat.sendMessage('🌧️ Оформите подписку с помощью команды /premium, чтобы иметь возможность выбирать модель нейронной сети.')
+			return
+		}
+
+		message.chat.sendMessage('Премиум подписка позволит вам выбирать более продвинутые модели нейронной сети, а также увеличит лимиты по длине запросов и ответов', {
+			replyMarkup: new InlineKeyboardMarkup({
+				inlineKeyboard: [[
+					new InlineKeyboardButton({
+						text: 'Gemini 1.5 Flash',
+						callbackData: 'model:flash'
+					})
+				], [
+					new InlineKeyboardButton({
+						text: 'Gemini 1.5 Pro',
+						callbackData: 'model:pro'
+					})
+				]]
+			})
+		})
+		return
+	}
+
 	try {
 		await message.chat.sendChatAction(ChatAction.Typing)
 		const result = await chatSession.sendMessage(message.text)
 		const resp = result.response.text()
 		let text = resp
-			// .replace(/\!/g, '\\!')
-			// .replace(/\./g, '\\.')
-			// .replace(/\?/g, '\\?')
-			// .replace(/\#/g, '\\#')
-			// .replace(/\(/g, '\\(')
-			// .replace(/\)/g, '\\)')
-			// .replace(/\-/g, '\\-')
-			// .replace(/\+/g, '\\+')
 		await message.chat.sendMessage(text, {
 			// parseMode: ParseMode.MarkdownV2
 		})
 	} catch (e) {
 		console.error(e)
-		message.chat.sendMessage('❌ Произошла ошибка')
+		message.chat.sendMessage('❌ Не удалось сгенерировать ответ')
+	}
+})
+
+bot.on('callbackQuery', async (query) => {
+	if (query.data?.startsWith('model')) {
+		const modelId = query.data.split(':')[1]
+		const modelName = modelId === 'flash' ? 'Gemini 1.5 Flash' : 'Gemini 1.5 Pro'
+		query.message.editText(`✅ Модель была изменена на ${modelName}`, {
+			replyMarkup: undefined
+		})
 	}
 })
 
 bot.on('preCheckoutQuery', async (query) => {
 	bot.answerPreCheckoutQuery(query.id, true)
-	await doc.loadInfo()
+	const { transactions } = await bot.getStarTransactions({})
+	// await bot.refundStarPayment(query.from.id, transactions.at(-1).id).catch(() => console.log)
 	const sheet = doc.sheetsByTitle['transactions']
 	await sheet.addRow({
 		userId: query.from.id,
-		date: new Date().getTime()
+		date: new Date().getTime(),
+		transactionId: transactions.at(-1).id
 	})
-	const { transactions } = await bot.getStarTransactions({
-	})
-	await bot.refundStarPayment(transactions.at(-1).id)
 })
 
 bot.on('successfulPayment', async (payment) => {
 	console.log(payment)
 })
 
-bot.polling.start()
+async function checkPremiumStatus(userId) {
+	const rows = await doc.sheetsByTitle['transactions'].getRows()
+	const transactionRow = rows.find((row) => row.get('userId') === userId.toString())
+	return Boolean(transactionRow)
+}
+
+doc.loadInfo().then(() => {
+	bot.polling.start();
+})
